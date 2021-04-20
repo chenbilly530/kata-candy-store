4ba65b709aae5f0f0f07dc8a127bafa52121f4d2
zijianchen <chenbilly97@gmail.com>
2021-04-20 16:05:43 -0400

    add initial candy service application

---
+++ candy-service/src/main/java/kata/telemetry2/candyservice/CandyServiceApplication.java
@@ -0,0 +1,13 @@
+package kata.telemetry2.candyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandyServiceApplication.class, args);
+    }
+
+}
---
+++ candy-service/src/test/java/kata/telemetry2/candyservice/CandyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.telemetry2.candyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
